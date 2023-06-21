7488998d10079d301ddeab199741f628688de781
Thiago Januario <tjanuario193@gmail.com>
2023-06-21 16:45:57 -0300

    projetoMVC

---
+++ CadastroClientes/CadastroClientes/Controllers/ClienteController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroClientes.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        public IActionResult Atualizar()
+        {
+            return View();
+        }
+        public IActionResult Apagar()
+        {
+            return View();
+        }
+    }
+}
